7e3bca9872c7b3b87cd0d12032f4062670dfcffa
Akshay Tenkale <tenkaleakshay94@gmail.com>
2018-11-08 23:41:09 -0500

    Create and get Project

---
+++ ppmtool/src/main/java/com/project/mamagement/ppmtool/Repository/ProjectRepository.java
@@ -0,0 +1,14 @@
+package com.project.mamagement.ppmtool.Repository;
+
+import com.project.mamagement.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+
+    Project findByProjectIdentifier(String projectId);
+}
---
+++ ppmtool/src/main/java/com/project/mamagement/ppmtool/services/ProjectService.java
@@ -0,0 +1,34 @@
+package com.project.mamagement.ppmtool.services;
+
+
+import com.project.mamagement.ppmtool.Repository.ProjectRepository;
+import com.project.mamagement.ppmtool.domain.Project;
+import com.project.mamagement.ppmtool.exceptions.ProjectIdException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project){
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
+    }
+    public Project findProjectByIdentifier(String projectId){
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project == null){
+            throw new ProjectIdException("Project ID '"+projectId+"' does not exist");
+        }
+        return project;
+    }
+
+}
